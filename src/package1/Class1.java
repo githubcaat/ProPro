76355094e2c3b85fa44745035c94d10ebb940b0d
githubcaat <napaydin00@gmail.com>
2021-03-13 19:59:47 -0500

    1

---
+++ src/package1/Class1.java
@@ -0,0 +1,12 @@
+package package1;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+
+	}
+
+}
